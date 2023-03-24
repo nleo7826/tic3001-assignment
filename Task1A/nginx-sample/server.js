9015ae261b92a588feeaac647fb9ff5bdf922cf9
Leonard <jsleo906@gmail.com>
2023-03-25 00:17:09 +0800

    Task 1B

---
+++ Task1A/nginx-sample/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
