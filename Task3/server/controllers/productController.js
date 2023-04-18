7594c9dc36039f88b68470ed9707b0d2236f7bea
Leonard <jsleo906@gmail.com>
2023-04-18 23:51:40 +0800

    upd

--- Task3/server/controllers/productController.js
+++ Task3/server/controllers/productController.js
@@ -26,81 +26,93 @@ exports.new = function (req, res) {
     product.name = req.body.name ? req.body.name : product.name;
     product.price = req.body.price;
     product.quantity = req.body.quantity;
-// save the product and check for errors
-    product.save(function (err) {
-        // Check for validation error
-        if (err)
-            res.json(err);
-        else
-            res.status(201).json({
-                message: 'New product created!',
-                data: product
-            });
-    });
-};
-// Handle view product info
-exports.view = function (req, res) {
-    Product.findById(req.params.product_id, function (err, product) {
-        if (err)
-            res.send(err);
-        res.json({
-            message: 'product details loading..',
-            data: product
+    // save the product and check for errors
+    product.save()
+      .then(() => {
+        res.status(201).json({
+          message: 'New product created!',
+          data: product
         });
-    });
-};
-// Handle update product info
-exports.update = function (req, res) {
-    Product.findById(req.params.product_id, function (err, product) {
-        if (err)
-            res.send(err);
+      })
+      .catch((err) => {
+        // Check for validation error
+        res.json(err);
+      });
+  };
+  
+  // Handle view product info
+  exports.view = function (req, res) {
+    Product.findById(req.params.product_id)
+      .then((product) => {
+        res.json({
+          message: 'product details loading..',
+          data: product
+        });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  };
+  
+  // Handle update product info
+  exports.update = function (req, res) {
+    Product.findById(req.params.product_id)
+      .then((product) => {
         product.name = req.body.name;
         product.price = req.body.price;
         product.quantity = req.body.quantity;
-// save the product and check for errors
-        product.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Product Info updated',
-                data: product
-            });
+        // save the product and check for errors
+        return product.save();
+      })
+      .then(() => {
+        res.json({
+          message: 'Product Info updated',
+          data: product
         });
-    });
-};
-// Handle patch product info
-exports.patch = function (req, res) {
-    Product.findById(req.params.product_id, function (err, product) {
-        if (err)
-            res.send(err);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  };
+  
+  // Handle patch product info
+  exports.patch = function (req, res) {
+    Product.findById(req.params.product_id)
+      .then((product) => {
         product.name = req.body.name ? req.body.name : product.name;
         product.price = req.body.price ? req.body.price : product.price;
         product.quantity = req.body.quantity ? req.body.quantity : product.quantity;
-// save the product and check for errors
-        product.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Product Info updated',
-                data: product
-            });
+        // save the product and check for errors
+        return product.save();
+      })
+      .then(() => {
+        res.json({
+          message: 'Product Info updated',
+          data: product
         });
-    });
-};
-// Handle delete product
-exports.delete = function (req, res) {
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  };
+  
+  // Handle delete product
+  exports.delete = function (req, res) {
     Product.deleteOne({
-        _id: req.params.product_id
-    }, function (err, product) {
-        if (err) {
-            res.status(404).send(err);
-        } else if (product.deletedCount === 0) {
-            res.status(404).send('Product not found');
+      _id: req.params.product_id
+    })
+      .then((product) => {
+        if (product.deletedCount === 0) {
+          res.status(404).send('Product not found');
         } else {
-            res.json({
-                status: "success",
-                message: 'Product successfully deleted'
-            });
+          res.json({
+            status: "success",
+            message: 'Product successfully deleted'
+          });
         }
-    });
-};
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+      });
+  };
+  
